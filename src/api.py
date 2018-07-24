061b201666ab2a38249209a5870615dfe9547cdd
Jacob <jacob@britecore.com>
2018-07-23 23:42:14 -0700

    Fix naming typo in api.py

--- src/api.py
+++ src/api.py
@@ -11,14 +11,14 @@ dynamodb = resource('dynamodb')
 books_table = dynamodb.Table(BOOKS_TABLE_NAME)
 
 
-@lambda_hander.handle('post', path='/books')
+@lambda_handler.handle('post', path='/books')
 def add_book(event):
     book_data = event['json']
     book_data['id'] = slugify(book_data['title'])
     books_table.put_item(Item=book_data)
 
 
-@lambda_hander.handle('get', path='/books/<str:book_id>/')
+@lambda_handler.handle('get', path='/books/<str:book_id>/')
 def get_book(event, book_id):
     book_details = books_table.get_item(
         Key={'id': book_id}
@@ -26,7 +26,7 @@ def get_book(event, book_id):
     return book_details['Item']
 
 
-@lambda_hander.handle('get', path='/books')
+@lambda_handler.handle('get', path='/books')
 def get_books(event, book_id):
     books = book_table.scan()
     return books['Items']
